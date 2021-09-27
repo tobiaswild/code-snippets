704357104928d8bfee52c423e3cc0a104694cc12
Tocoolmh58 <tobias.m.wild@gmail.com>
2021-09-27 21:41:23 +0200

    breadcrumbs (does not work)

--- source/breadcrumbs/script.js
+++ source/breadcrumbs/script.js
@@ -1,1 +1,21 @@
-js
+$(document).ready(function () {
+
+    var url = window.location.href;
+    var parts = url.split("/");
+    var crumbsText = "";
+    var backText = "";
+    var backLength = parts.length-1-3;
+
+    for (var k = 0; k < backLength; i++) {
+        backText += "../"
+    }
+
+    for (var i = 3; i < parts.length-1; i++) {
+        console.log(parts[i]);
+        console.log(backText);
+        crumbsText +=  " > " + '<a href="../' + backText + parts[i] + '/">' + parts[i] + '</a>';
+        $("#crumbs").html(crumbsText);
+        backText = backText - "../";
+    }
+
+})
