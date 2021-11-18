61ebf7b7edbaa7fb00e5e60f9afca3c8caade8b9
Tocoolmh58 <tobias.m.wild@gmail.com>
2021-11-18 13:03:57 +0100

    fix breadcrumbs

--- js/breadcrums.js
+++ js/breadcrums.js
@@ -1,21 +1,24 @@
-$(document).ready(function () {
+window.onload = function () {
+  var url = window.location.href;
+  var parts = url.split("/");
+  let baseUrl = "";
+  let crumbs = "";
+  let allCrumbs = "";
 
-    var url = window.location.href;
-    var parts = url.split("/");
-    var crumbsText = "";
-    var backText = "";
-    var backLength = parts.length-1-3;
+  for (let i = 0; i < 3; i++) {
+    baseUrl += parts[i] + "/";
+  }
 
-    for (var k = 0; k < backLength; i++) {
-        backText += "../"
-    }
+  for (let i = 3; i < parts.length - 1; i++) {
+    allCrumbs += parts[i] + "/";
+    crumbs += `<a href="${baseUrl + allCrumbs}"/>${parts[i]}</a> > `;
+  }
+  crumbs += `<a href="${baseUrl + allCrumbs + parts[parts.length - 1]}"/>${
+    parts[parts.length - 1]
+  }</a>`;
 
-    for (var i = 3; i < parts.length-1; i++) {
-        console.log(parts[i]);
-        console.log(backText);
-        crumbsText +=  " > " + '<a href="../' + backText + parts[i] + '/">' + parts[i] + '</a>';
-        $("#crumbs").html(crumbsText);
-        backText = backText - "../";
-    }
+  console.log(baseUrl);
+  console.log(crumbs);
 
-})
+  document.getElementById("test").innerHTML = crumbs;
+};
